94ff3c8b704817620cb1fbc2619700d0d131d682
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-03-09 12:17:05 +0100

    Register assets

--- src/UmbrellaBundle.php
+++ src/UmbrellaBundle.php
@@ -5,6 +5,7 @@ namespace Torr\Umbrella;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Torr\Assets\Container\RegisterAssetNamespaceCompilerPass;
 use Torr\BundleHelpers\Bundle\ConfigurableBundleExtension;
 use Torr\Umbrella\Component\Library\ComponentLibraryLoader;
 use Torr\Umbrella\DependencyInjection\UmbrellaBundleConfig;
@@ -31,7 +32,19 @@ final class UmbrellaBundle extends Bundle
 		);
 	}
 
-	/**
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new RegisterAssetNamespaceCompilerPass(
+            "umbrella",
+            \dirname(__DIR__) . "/build"
+        ));
+    }
+
+
+    /**
 	 * @inheritDoc
 	 */
 	public function getPath () : string
